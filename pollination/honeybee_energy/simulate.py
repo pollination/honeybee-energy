dd9f11ac51e8419d82e286de191ad60252947b02
Chris Mackey <chris@ladybug.tools>
2021-09-21 22:12:23 -0400

    fix(simulate): Add inputs for measures and report inputs

--- pollination/honeybee_energy/simulate.py
+++ pollination/honeybee_energy/simulate.py
@@ -26,6 +26,12 @@ class SimulateModel(Function):
         'simulation.', path='sim-par.json', extensions=['json'], optional=True
     )
 
+    measures = Inputs.folder(
+        description='A folder containing an OSW JSON be used as the base for the '
+        'execution of the OpenStuduo CLI. This folder must also contain all of the '
+        'measures that are referenced within the OSW.', path='measures', optional=True
+    )
+
     additional_string = Inputs.str(
         description='An additional text string to be appended to the IDF before '
         'simulation. The input should include complete EnergyPlus objects as a '
@@ -33,11 +39,27 @@ class SimulateModel(Function):
         'EnergyPlus objects that are not currently supported by honeybee.', default=''
     )
 
+    report_units = Inputs.str(
+        description='A switch to indicate whether the data in the output HTML report '
+        'should be in SI or IP units. Valid values are "si" and "ip" and "none". '
+        'If "none", no HTML report will be produced.', default='none',
+        spec={'type': 'string', 'enum': ['none', 'si', 'ip']}
+    )
+
+    viz_variables = Inputs.str(
+        description='Text for EnergyPlus output variables to be visualized on the '
+        'geometry in an output view_data HTML report. If unspecified, no view_data '
+        'report is produced. Each variable should be in "quotes" and should be '
+        'preceded by a -v option. For example\n-v "Zone Air System Sensible Heating '
+        'Rate" -v "Zone Air System Sensible Cooling Rate".', default=''
+    )
+
     @command
     def simulate_model(self):
         return 'honeybee-energy simulate model model.hbjson weather.epw ' \
-            '--sim-par-json sim-par.json --additional-string ' \
-            '"{{self.additional_string}}" --folder output'
+            '--sim-par-json sim-par.json --measures measures --additional-string ' \
+            '"{{self.additional_string}}" --report-units {{self.report_units}} ' \
+            '--folder output {{self.viz_variables}}'
 
     result_folder = Outputs.folder(
         description='Folder containing all simulation result files.',
@@ -78,6 +100,18 @@ class SimulateModel(Function):
     err = Outputs.file(
         description='The error report output by the simulation.',
         path='output/run/eplusout.err'
+    )
+
+    result_report = Outputs.file(
+        description='The HTML report with interactive summaries of energy use, '
+        'HVAC component sizes, and other information.', optional=True,
+        path='reports/openstudio_results_report.html'
+    )
+
+    visual_report = Outputs.file(
+        description='The HTML report with hourly EnergyPlus output variables '
+        'visualized on the geometry.', optional=True,
+        path='reports/view_data_report.html'
     )
 
 
