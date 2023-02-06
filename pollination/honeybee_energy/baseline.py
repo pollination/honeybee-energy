f064f1bb87aba617473de9ce8ef856a92d419542
Chris Mackey <chris@ladybug.tools>
2023-02-06 11:06:09 -0500

    fix(baseline): Refactor baseline for the latest Appendix G

--- pollination/honeybee_energy/baseline.py
+++ pollination/honeybee_energy/baseline.py
@@ -3,41 +3,27 @@ from pollination_dsl.function import Inputs, Outputs, Function, command
 
 
 @dataclass
-class Geometry2004(Function):
-    """Convert a Model's geometry to conform to ASHRAE 90.1-2004 appendix G.
+class ModelToBaseline(Function):
+    """Convert a Model to be conformant with ASHRAE 90.1 appendix G.
 
-    This includes stripping out all attached shades (leaving detached shade as
-    context), reducing the vertical glazing ratio to 40% it it's above this value,
-    and reducing the skylight ratio to 5% of it's above this value.
+    This includes running all functions to adjust the geometry, constructions,
+    lighting, HVAC, SHW, and remove any clearly-defined energy conservation
+    measures like daylight controls. Note that all schedules are essentially
+    unchanged, meaning that additional post-processing of setpoints may be
+    necessary to account for energy conservation strategies like expanded
+    comfort ranges, ceiling fans, and personal thermal comfort devices. It may
+    also be necessary to adjust electric equipment loads in cases where such
+    equipment qualifies as an energy conservation strategy or hot water loads in
+    cases where low-flow fixtures are implemented.
+
+    Note that not all versions of ASHRAE 90.1 use this exact definition of a
+    baseline model but version 2016 and onward conform to it. It is essentially
+    an adjusted version of the 90.1-2004 methods.
     """
 
     model = Inputs.file(
-        description='Honeybee model in JSON format.', path='model.hbjson',
-        extensions=['hbjson', 'json']
-    )
-
-    @command
-    def geometry_2004(self):
-        return 'honeybee-energy baseline geometry-2004 model.hbjson ' \
-            '--output-file new_model.hbjson'
-
-    new_model = Outputs.file(
-        description='Model JSON with its properties edited to conform to ASHRAE '
-        '90.1 appendix G.', path='new_model.hbjson'
-    )
-
-
-@dataclass
-class Constructions2004(Function):
-    """Convert a Model's constructions to conform to ASHRAE 90.1-2004 appendix G.
-
-    This includes assigning a ConstructionSet that is compliant with Table 5.5 to
-    all rooms in the model.
-    """
-
-    model = Inputs.file(
-        description='Honeybee model in JSON format.', path='model.hbjson',
-        extensions=['hbjson', 'json']
+        description='Honeybee model.', path='model.hbjson',
+        extensions=['hbjson', 'json', 'hbpkl', 'pkl']
     )
 
     climate_zone = Inputs.str(
@@ -47,90 +33,24 @@ class Constructions2004(Function):
         spec={
             'type': 'string',
             'enum': [
-                '1', '2', '3', '4', '5', '6', '7', '8',
-                '1A', '2A', '3A', '4A', '5A', '6A', '7A', '8A',
-                '1B', '2B', '3B', '4B', '5B', '6B', '7B', '8B',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                '0A', '1A', '2A', '3A', '4A', '5A', '6A',
+                '0B', '1B', '2B', '3B', '4B', '5B', '6B',
                 '3C', '4C', '5C'
             ]
         }
     )
 
-    @command
-    def constructions_2004(self):
-        return 'honeybee-energy baseline constructions-2004 model.hbjson ' \
-            '{{self.climate_zone}} --output-file new_model.hbjson'
-
-    new_model = Outputs.file(
-        description='Model JSON with its properties edited to conform to ASHRAE '
-        '90.1 appendix G.', path='new_model.hbjson'
-    )
-
-
-@dataclass
-class Lighting2004(Function):
-    """Convert a Model's lighting to conform to ASHRAE 90.1-2004 appendix G.
-
-    This includes determining whether an ASHRAE 2004 equivalent exists for each
-    program type in the model. If none is found, the baseline_watts_per_area on
-    the room's program's lighting will be used, which will default to a typical
-    office if none has been specified.
-    """
-
-    model = Inputs.file(
-        description='Honeybee model in JSON format.', path='model.hbjson',
-        extensions=['hbjson', 'json']
-    )
-
-    @command
-    def lighting_2004(self):
-        return 'honeybee-energy baseline lighting-2004 model.hbjson ' \
-            '--output-file new_model.hbjson'
-
-    new_model = Outputs.file(
-        description='Model JSON with its properties edited to conform to ASHRAE '
-        '90.1 appendix G.', path='new_model.hbjson'
-    )
-
-
-@dataclass
-class Hvac2004(Function):
-    """Convert a Model's HVAC to conform to ASHRAE 90.1-2004 appendix G.
-
-    This includes the selection of the correct Appendix G template HVAC based on
-    the inputs and the application of this HVAC to all conditioned spaces in
-    the model.
-    """
-
-    model = Inputs.file(
-        description='Honeybee model in JSON format.', path='model.hbjson',
-        extensions=['hbjson', 'json']
-    )
-
-    climate_zone = Inputs.str(
-        description='Text indicating the ASHRAE climate zone. This can be a single '
-        'integer (in which case it is interpreted as A) or it can include the '
-        'A, B, or C qualifier (eg. 3C).',
-        spec={
-            'type': 'string',
-            'enum': [
-                '1', '2', '3', '4', '5', '6', '7', '8',
-                '1A', '2A', '3A', '4A', '5A', '6A', '7A', '8A',
-                '1B', '2B', '3B', '4B', '5B', '6B', '7B', '8B',
-                '3C', '4C', '5C'
-            ]
-        }
-    )
-
-    is_residential = Inputs.str(
-        description='A switch to note whether the model represents a residential '
-        'or nonresidential building.', default='nonresidential',
-        spec={'type': 'string', 'enum': ['nonresidential', 'residential']}
-    )
-
-    energy_source = Inputs.str(
-        description='A switch to note whether the available energy source is '
-        'fossil fuel based or all-electric.', default='fuel',
-        spec={'type': 'string', 'enum': ['fuel', 'electric']}
+    building_type = Inputs.str(
+        description='Text for the building type that the Model represents. This is used '
+        'to determine the baseline window-to-wall ratio and HVAC system. If the type is '
+        'not recognized or is "Unknown", it will be assumed that the building is generic'
+        ' NonResidential. The following have specified systems per the standard: '
+        'Residential, NonResidential, MidriseApartment, HighriseApartment, LargeOffice, '
+        'MediumOffice, SmallOffice, Retail, StripMall, PrimarySchool, SecondarySchool, '
+        'SmallHotel, LargeHotel, Hospital, Outpatient, Warehouse, SuperMarket, '
+        'FullServiceRestaurant, QuickServiceRestaurant, Laboratory',
+        default='Unknown'
     )
 
     floor_area = Inputs.float(
@@ -144,38 +64,26 @@ class Hvac2004(Function):
         spec={'type': 'integer', 'minimum': 0}
     )
 
+    lighting_method = Inputs.str(
+        description='A switch to note whether the building-type should be used to '
+        'assign the baseline lighting power density, which will use the same value '
+        'for all Rooms in the model, or a space-by-space method should be used. '
+        'To use the space-by-space method, the model should either be built '
+        'with the programs that ship with Ladybug Tools in honeybee-energy-standards '
+        'or the baseline_watts_per_area should be correctly '
+        'assigned for all Rooms.', default='space',
+        spec={'type': 'string', 'enum': ['space', 'building']}
+    )
+
     @command
-    def hvac_2004(self):
-        return 'honeybee-energy baseline hvac-2004 model.hbjson {{self.climate_zone}} ' \
-            '--{{self.is_residential}} --{{self.energy_source}} ' \
+    def create_baseline(self):
+        return 'honeybee-energy baseline create model.hbjson {{self.climate_zone}} ' \
+            '--building-type "{{self.building_type}}" ' \
             '--story-count {{self.story_count}} --floor-area {{self.floor_area}} ' \
-            '--output-file new_model.hbjson'
+            '--lighting-by-{{self.lighting_method}} ' \
+            '--output-file baseline_model.hbjson'
 
-    new_model = Outputs.file(
+    baseline_model = Outputs.file(
         description='Model JSON with its properties edited to conform to ASHRAE '
-        '90.1 appendix G.', path='new_model.hbjson'
-    )
-
-
-@dataclass
-class RemoveEcms(Function):
-    """Remove energy conservation strategies (ECMs) not associated with baseline models.
-
-    This includes removing the opening behavior of all operable windows, daylight
-    controls, etc.
-    """
-
-    model = Inputs.file(
-        description='Honeybee model in JSON format.', path='model.hbjson',
-        extensions=['hbjson', 'json']
-    )
-
-    @command
-    def remove_ecms(self):
-        return 'honeybee-energy baseline remove-ecms model.hbjson ' \
-            '--output-file new_model.hbjson'
-
-    new_model = Outputs.file(
-        description='Model JSON with its properties edited to conform to ASHRAE '
-        '90.1 appendix G.', path='new_model.hbjson'
+        '90.1 appendix G.', path='baseline_model.hbjson'
     )
--- tests/baseline_test.py
+++ tests/baseline_test.py
@@ -1,33 +1,8 @@
-from pollination.honeybee_energy.baseline import Geometry2004, Constructions2004, \
-    Lighting2004, Hvac2004, RemoveEcms
+from pollination.honeybee_energy.baseline import ModelToBaseline
 from queenbee.plugin.function import Function
 
 
-def test_geometry2004():
-    function = Geometry2004().queenbee
-    assert function.name == 'geometry2004'
-    assert isinstance(function, Function)
-
-
-def test_constructions2004():
-    function = Constructions2004().queenbee
-    assert function.name == 'constructions2004'
-    assert isinstance(function, Function)
-
-
-def test_lighting2004():
-    function = Lighting2004().queenbee
-    assert function.name == 'lighting2004'
-    assert isinstance(function, Function)
-
-
-def test_hvac2004():
-    function = Hvac2004().queenbee
-    assert function.name == 'hvac2004'
-    assert isinstance(function, Function)
-
-
-def test_remove_ecms():
-    function = RemoveEcms().queenbee
-    assert function.name == 'remove-ecms'
+def test_model_to_baseline():
+    function = ModelToBaseline().queenbee
+    assert function.name == 'model-to-baseline'
     assert isinstance(function, Function)
