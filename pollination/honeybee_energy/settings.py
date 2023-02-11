9992c9ae16137944e77b9fe2e651c476d9891c9d
Chris Mackey <chris@ladybug.tools>
2023-02-11 16:16:28 -0500

    fix(settings): Add inputs for efficiency standards
    These will be used in the latest Appendix G recipe.

--- pollination/honeybee_energy/settings.py
+++ pollination/honeybee_energy/settings.py
@@ -11,6 +11,16 @@ class SimParDefault(Function):
         'SimulationParameter', path='input.ddy', extensions=['ddy'], optional=True
     )
 
+    reporting_frequency = Inputs.str(
+        description='Text for the frequency at which the outputs are reported. '
+        '(Default: Hourly). Choose from the following: Annual, Monthly, Daily, '
+        'Hourly, Timestep', default='Hourly',
+        spec={
+            'type': 'string',
+            'enum': ['Annual', 'Monthly', 'Daily', 'Hourly', 'Timestep']
+        }
+    )
+
     run_period = Inputs.str(
         description='An AnalysisPeriod string or an IDF RunPeriod string to set the '
         'start and end dates of the simulation (eg. "6/21 to 9/21 between 0 and 23 @1").'
@@ -29,10 +39,41 @@ class SimParDefault(Function):
         spec={'type': 'string', 'enum': ['filter-des-days', 'all-des-days']}
     )
 
+    efficiency_standard = Inputs.str(
+        description='Text to set the efficiency standard, which will '
+        'automatically set the efficiencies of all HVAC equipment when provided. '
+        'Note that providing a standard here will cause the OpenStudio translation '
+        'process to perform an additional sizing calculation with EnergyPlus, '
+        'which is needed since the default efficiencies of equipment vary depending on '
+        'their size. THIS WILL SIGNIFICANTLY INCREASE TRANSLATION TIME TO OPENSTUDIO. '
+        'However, it is often worthwhile when the goal is to match the '
+        'HVAC specification with a particular standard.Choose from the following: '
+        'DOE_Ref_Pre_1980, DOE_Ref_1980_2004, ASHRAE_2004, ASHRAE_2007, ASHRAE_2010, '
+        'ASHRAE_2013, ASHRAE_2016, ASHRAE_2019', default=''
+    )
+
+    climate_zone = Inputs.str(
+        description='Text indicating the ASHRAE climate zone to be used with the '
+        'efficiency_standard. When unspecified, the climate zone will be '
+        'inferred from the design days. This input can be a single integer (in which '
+        'case it is interpreted as A) or it can include the A, B, or C qualifier '
+        '(eg. 3C).', default=''
+    )
+
+    building_type = Inputs.str(
+        description='Text for the building type to be used in the efficiency_standard. '
+        'If the type is not recognized or is None, it will be assumed that '
+        'the building is a generic NonResidential.', default=''
+    )
+
     @command
     def create_sim_param(self):
         return 'honeybee-energy settings default-sim-par input.ddy ' \
+            '--reporting-frequency {{self.reporting_frequency}} ' \
             '--run-period "{{self.run_period}}" --north {{self.north}} ' \
+            '--efficiency-standard "{{self.efficiency_standard}}" ' \
+            '--climate-zone "{{self.climate_zone}}" ' \
+            '--building-type "{{self.building_type}}" ' \
             '--{{self.filter_des_days}} --output-file sim_par.json'
 
     sim_par_json = Outputs.file(
@@ -118,6 +159,16 @@ class BaselineOrientationSimPars(Function):
         'SimulationParameter', path='input.ddy', extensions=['ddy'], optional=True
     )
 
+    reporting_frequency = Inputs.str(
+        description='Text for the frequency at which the outputs are reported. '
+        '(Default: Hourly). Choose from the following: Annual, Monthly, Daily, '
+        'Hourly, Timestep', default='Hourly',
+        spec={
+            'type': 'string',
+            'enum': ['Annual', 'Monthly', 'Daily', 'Hourly', 'Timestep']
+        }
+    )
+
     run_period = Inputs.str(
         description='An AnalysisPeriod string or an IDF RunPeriod string to set the '
         'start and end dates of the simulation (eg. "6/21 to 9/21 between 0 and 23 @1").'
@@ -136,11 +187,42 @@ class BaselineOrientationSimPars(Function):
         spec={'type': 'string', 'enum': ['filter-des-days', 'all-des-days']}
     )
 
+    efficiency_standard = Inputs.str(
+        description='Text to set the efficiency standard, which will '
+        'automatically set the efficiencies of all HVAC equipment when provided. '
+        'Note that providing a standard here will cause the OpenStudio translation '
+        'process to perform an additional sizing calculation with EnergyPlus, '
+        'which is needed since the default efficiencies of equipment vary depending on '
+        'their size. THIS WILL SIGNIFICANTLY INCREASE TRANSLATION TIME TO OPENSTUDIO. '
+        'However, it is often worthwhile when the goal is to match the '
+        'HVAC specification with a particular standard.Choose from the following: '
+        'DOE_Ref_Pre_1980, DOE_Ref_1980_2004, ASHRAE_2004, ASHRAE_2007, ASHRAE_2010, '
+        'ASHRAE_2013, ASHRAE_2016, ASHRAE_2019', default=''
+    )
+
+    climate_zone = Inputs.str(
+        description='Text indicating the ASHRAE climate zone to be used with the '
+        'efficiency_standard. When unspecified, the climate zone will be '
+        'inferred from the design days. This input can be a single integer (in which '
+        'case it is interpreted as A) or it can include the A, B, or C qualifier '
+        '(eg. 3C).', default=''
+    )
+
+    building_type = Inputs.str(
+        description='Text for the building type to be used in the efficiency_standard. '
+        'If the type is not recognized or is None, it will be assumed that '
+        'the building is a generic NonResidential.', default=''
+    )
+
     @command
     def baseline_orientation_sim_pars(self):
         return 'honeybee-energy settings orientation-sim-pars input.ddy ' \
-            '0 90 180 270 --run-period "{{self.run_period}}" --start-north ' \
-            '{{self.north}} --{{self.filter_des_days}} --folder output ' \
+            '0 90 180 270 --reporting-frequency {{self.reporting_frequency}} ' \
+            '--run-period "{{self.run_period}}" --start-north {{self.north}} ' \
+            '--efficiency-standard "{{self.efficiency_standard}}" ' \
+            '--climate-zone "{{self.climate_zone}}" ' \
+            '--building-type "{{self.building_type}}" ' \
+            '--{{self.filter_des_days}} --folder output ' \
             '--log-file output/sim_par_info.json'
 
     sim_par_list = Outputs.dict(
